2c920e51ecf09c1b050fe86e9b8fb5c21906e8f7
MartinZilber <48520155@est.ort.edu.ar>
2025-03-25 09:58:31 -0300

    Añadir inputs - 1.0

--- codigo.js
+++ codigo.js
@@ -1,8 +1,30 @@
-let items = ["respirar", "vivir", "pestanear"];
+let items = [];
 const contenedor = document.getElementsByClassName("items");
+const botonEnviar = document.getElementById('boton');
+
+const anadirItem = () =>{
+    let objeto = {
+        tarea:document.getElementById('descripcion').value,
+        fechaCreacion:new Date(),
+        realizado:false,
+        fechaRealizacion:null
+    }
+    items.push(objeto);
+    console.log(items);
+    refrescarPagina();
+}
+const refrescarPagina = () =>{
+    items.forEach(item =>{
+        let contenedorItem = document.createElement("p");
+        contenedorItem.innerText = item.tarea;
+        contenedor.appendChild(contenedorItem);
+    })
+}
+
 
 for (let i = 0; i < items.length; i++)
 {
     
 
 }
+
